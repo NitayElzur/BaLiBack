2bab966526c46e4ac1ca487d6add50af3ba3dca4
unknown <elzur.nitay@gmail.com>
2023-07-25 10:12:23 +0300

    adjusted object return

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const google = require('@googleapis/youtube');
 const youtube = google.youtube({ version: 'v3', auth: process.env.AUTH_KEY })
 exports.getPopularInIsrael = async (req, res) => {
@@ -14,5 +15,17 @@ exports.getPopularInIsrael = async (req, res) => {
         playlistId: playlistId.data.items[0].id.playlistId,
         maxResults: 20,
     })
-        .then(({data}) => res.status(200).json(data.items))
+        // .then(({data}) => res.status(200).send(data.items))
+        .then(({ data }) => res.status(200).send(data.items.map(v => {
+            return (
+                {
+                    name: v.snippet.title,
+                    artist: v.snippet.videoOwnerChannelTitle,
+                    url: `https://www.youtube.com/watch?v=${v.snippet.resourceId.videoId}`,
+                    img: v.snippet.thumbnails.default.url,
+                    uploaded: v.snippet.publishedAt,
+                    timeSent: new Date()
+                }
+            )
+        })))
 }
