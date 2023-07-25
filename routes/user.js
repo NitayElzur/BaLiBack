5e3bcf2ec301c345474e46439df5c1cfc8e179df
Yoavelko <yoavelkobi889@gmail.com>
2023-07-25 16:10:24 +0300

    25.07

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const { getPlaylist, getDummyData } = require('../controllers/user');
 
 router.get('/playlist/:type', getPlaylist);
 router.get('/dummy', getDummyData);
+router.post('/search', searchSong);
 
 module.exports = router;
