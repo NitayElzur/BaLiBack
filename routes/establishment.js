1eb4422df7e3cfa0b4dacf8a105b42a128fa1cbb
Nitay Elzur <elzur.nitay@gmail.com>
2023-07-27 16:32:12 +0300

    final for 27/07

--- routes/establishment.js
+++ routes/establishment.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const { fetchAll, create, fetchSpecific, updateEstablishment } = require('../controllers/establishment')
+const { fetchAll, create, fetchSpecific, updateEstablishment, acceptSong, getAccepted, getRequested } = require('../controllers/establishment')
 
 router.get('/fetch-all', fetchAll);
 router.post('/create', create);
 router.post('/fetch-specific', fetchSpecific);
 router.patch('/update', updateEstablishment);
+router.post('/requested', getRequested);
+router.patch('/accept', acceptSong);
+router.post('/accepted', getAccepted);
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPlaylist, getDummyData, searchSong, sendSong, acceptSong, getRequested, getAccepted, createNewUser } = require('../controllers/user');
+const { getPlaylist, getDummyData, searchSong, sendSong, createNewUser } = require('../controllers/user');
 
 router.get('/new-user', createNewUser);
 router.get('/playlist/:type', getPlaylist);
 router.post('/search', searchSong);
 router.get('/dummy/:type', getDummyData);
 router.patch('/send', sendSong);
-router.post('/requested', getRequested);
-router.patch('/accept', acceptSong);
-router.post('/accepted', getAccepted);
 
 module.exports = router;
