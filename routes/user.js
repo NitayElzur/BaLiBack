eae7ae7261d694a1978d2754d2a4984d1d548089
Yoavelko <yoavelkobi889@gmail.com>
2023-07-27 13:57:43 +0300

    27/07

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPlaylist, getDummyData, searchSong, sendSong, acceptSong, getRequested, getAccepted } = require('../controllers/user');
+const { getPlaylist, getDummyData, searchSong, sendSong } = require('../controllers/user');
 
 router.get('/playlist/:type', getPlaylist);
 router.post('/search', searchSong);
 router.get('/dummy/:type', getDummyData);
 router.patch('/send', sendSong);
-router.post('/requested', getRequested);
-router.patch('/accept', acceptSong);
-router.post('/accepted', getAccepted)
+
 
 module.exports = router;
