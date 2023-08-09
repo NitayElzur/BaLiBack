70b2588434a8c1f7533ff795793e04d84389b027
Nitay Elzur <elzur.nitay@gmail.com>
2023-08-09 21:07:00 +0300

    admin send song and fix for playlist export

--- routes/establishment.js
+++ routes/establishment.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { fetchAll, create, fetchSpecific, updateEstablishment, acceptSong, getRequested, getAccepted, removeRequest, removeAccept, getSongsFromPlaylist, pushToPlayed, changeAccepted, getEstabBest, changeRequested } = require('../controllers/establishment')
+const { fetchAll, create, fetchSpecific, updateEstablishment, acceptSong, getRequested, getAccepted, removeRequest, removeAccept, adminSendSong, getSongsFromPlaylist, pushToPlayed, changeAccepted, getEstabBest, changeRequested } = require('../controllers/establishment')
 
 router.get('/fetch-all', fetchAll);
 router.post('/create', create);
@@ -15,5 +15,6 @@ router.post('/getEstabBest', getEstabBest);
 router.post('/change-requested', changeRequested);
 router.post('/get-songs-from-playlist', getSongsFromPlaylist);
 router.post('/push-to-played', pushToPlayed);
+router.post('/admin-send-song', adminSendSong);
 
 module.exports = router
